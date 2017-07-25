5ab9b5794e0b5fdf24819ae89d78ecb63c20330d
hackerchai <admin@hackerchai.com>
2017-07-25 15:42:35 +0800

    check python serial module

--- serial.py
+++ serial.py
@@ -1,14 +1,21 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
+#!/usr/bin/python 
+# -*- coding: utf-8 -*
 import serial
+import time
 
-ser = serial.Serial("/dev/ttyS0",115200)
-
-print('serial test start ...')
-ser.write("Hello Wrold !!!\n")
-try:
-        while True:
-                    ser.write(ser.read())
-except KeyboardInterrupt:
+ser = serial.Serial("/dev/ttyS0", 115200)
+def main():
+    while True:
+        count = ser.inWaiting()
+        if count != 0:
+            recv = ser.read(count)
+            ser.write(recv)
+        ser.flushInput()
+        time.sleep(0.1)
+   
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
         if ser != None:
-                    ser.close()
+            ser.close()
